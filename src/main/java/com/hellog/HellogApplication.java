a2c91e97837c4af3b5064c8c1fb1b76696aac03d
whitebear05 <woojs0505@naver.com>
2022-08-29 11:12:26 +0900

    :tada: :: start project

---
+++ src/main/java/com/hellog/HellogApplication.java
@@ -0,0 +1,13 @@
+package com.hellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellog/HellogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
