695deda5fdb82b99f75abf39edd9e90dc808e7cd
whitebear05 <woojs0505@naver.com>
2022-10-21 11:26:26 +0900

    :memo: :: PostingController 구현

--- src/main/java/com/hellog/domain/posting/presentation/PostingController.java
+++ src/main/java/com/hellog/domain/posting/presentation/PostingController.java
@@ -1,10 +1,16 @@
 package com.hellog.domain.posting.presentation;
 
+import com.hellog.domain.posting.domain.entity.Posting;
+import com.hellog.domain.posting.presentation.dto.request.CreatePostingRequest;
+import com.hellog.domain.posting.presentation.dto.request.UpdatePostingRequest;
 import com.hellog.domain.posting.service.PostingService;
+import com.hellog.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/posting")
@@ -12,4 +18,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostingController {
 
     private final PostingService postingService;
+
+    @GetMapping("/trending")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Posting> getTrendingPostings() {
+        return postingService.getTrendingPosting();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Posting getPostingById(@PathVariable("id") long postingId) {
+        return postingService.getPostingById(postingId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Posting createPosting(
+            @RequestBody @Valid CreatePostingRequest request,
+            @RequestAttribute User user
+    ) {
+        return postingService.createPosting(request, user);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Posting updatePosting(
+            @RequestBody @Valid UpdatePostingRequest request,
+            @RequestAttribute User user
+    ) {
+        return postingService.updatePosting(request, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deletePosting(
+            @PathVariable("id") long postingId,
+            @RequestAttribute User user
+    ) {
+        postingService.deletePosting(postingId, user);
+    }
 }
